0b2c76e191d40cb9e3c1fcce1b0376f233da21f9
alvafi <150460935+alvafi@users.noreply.github.com>
2024-05-14 11:00:38 +0500

    Update views.sql

--- views.sql
+++ views.sql
@@ -2,4 +2,4 @@ CREATE VIEW Car_Info AS
 SELECT c.id, c.state_number AS car_state_number, c.status AS car_satus,
        r.cost AS rate_cost
 FROM Car c
-JOIN rate r ON c.id_parent = r.id_parent;
+JOIN Rate r ON c.id_parent = r.id_parent;
